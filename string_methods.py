e88c4d8dad0c4f5d41bbdcb2a9384be332d52819
Aashish012-rgb <182605557+Aashish012-rgb@users.noreply.github.com>
2025-12-26 20:16:21 +0545

    added new python practiced files

--- string_methods.py
+++ string_methods.py
@@ -1,17 +1,22 @@
-#string metods 
-name = input("Enter your full name:")
+# #string metods 
+# name = input("Enter your full name:")
 
-result =len(name)
-result = name.find("A") #for first occurence
-answer = name.rfind("h")#for last occurence
-name = name.capitalize() #to capitalize
-print(result)# for first occurence
-print(answer) #for last occurence
-print (name) #for capitalize
-aashish = name.upper()
-print(aashish)
-digit = name.isdigit()
-print(digit)
-# Treat full name as alphabetic if every word is alphabetic (ignores spaces)
-alpha = all(part.isalpha() for part in name.split())
-print(alpha)
+# result =len(name)
+# result = name.find("A") #for first occurence
+# answer = name.rfind("h")#for last occurence
+# name = name.capitalize() #to capitalize
+# print(result)# for first occurence
+# print(answer) #for last occurence
+# print (name) #for capitalize
+# aashish = name.upper()
+# print(aashish)
+# digit = name.isdigit()# to check whether there is digit or not 
+# print(digit)
+# alpha = name.isalpha()# to check whether there  is alphabet of not  
+# print(alpha)
+# # output = false because it contains false 
+phone_number =input("Enter your phone number:")
+
+# result= phone_number.count("-") #use to count
+result= phone_number.replace("-"," ")
+print(result) 
